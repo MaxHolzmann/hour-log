1a7648326a4d3a7c2ecc9ebb650eeb5cbed7a919
MaxHolzmann <max@holzmann.io>
2024-07-26 13:39:58 -0500

    changes not logged in screen to link to auth route

--- components/NotLoggedIn.js
+++ components/NotLoggedIn.js
@@ -5,11 +5,8 @@ const NotLoggedIn = () => {
     <>
       <div className="flex flex-col justify-center items-center w-screen h-screen gap-10">
         <h1 className="text-7xl">Not Logged in!</h1>
-        <button
-          className="bg-red-500 text-white hover:text-red-500 hover:bg-white hover:border-red-500 border-2 border-red-500 rounded-md p-2"
-          onClick={() => signIn()}
-        >
-          Sign in with Google
+        <button className="bg-red-500 text-white hover:text-red-500 hover:bg-white hover:border-red-500 border-2 border-red-500 rounded-md p-2">
+          <a href="/api/auth/signin">Sign in with Google</a>
         </button>
       </div>
     </>
